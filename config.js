a434891a40c365954aadf31b59abd094e692d4e6
Beacon Street Studios <143728792+Beacon-Street-Studios@users.noreply.github.com>
2023-12-16 15:19:54 -0500

    trying to move main stack to the left on the stage.

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ config.defaultPlaybackRate = 1.0;
 
 // seconds as min/max bounds, min default 0, max default config.duration
 config.voices = [
-    {image: 'Lickin.png', file: 'bss_kfc_lickinlab_lick_', count:26, time: 1.45, velocity: 0.8, y: 62},
+    {image: 'Lickin.png', file: 'bss_kfc_lickinlab_lick_', count:26, time: 1.0, velocity: 0.8, y: 62},
     {image: 'Mmmm.png', file: 'bss_kfc_lickinlab_mmm_', count:26, time: 1.45, velocity: 0.8, y: 413},  
     {image: 'Music.png', file: 'bss_kfc_lickinlab_music_', count:26, time: 1.45, velocity: 0.8, y: 722}, 
     {image: 'X-Factor.png', file: 'bss_kfc_lickinlab_xfactor_', count:26, min:[0.0,2.3], max:[0.6,3.2]}
