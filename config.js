01d5ed590a8f3e790810a624a6f8d0b0a54fd6b1
Beacon Street Studios <143728792+Beacon-Street-Studios@users.noreply.github.com>
2024-01-17 08:25:08 -0500

    Update config.js
    Updating number of assets to 30

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ config.defaultPlaybackRate = 1.2;
 
 // seconds as min/max bounds, min default 0, max default config.duration
 config.voices = [
-    {image: 'BWTM.png', file: 'bss_marcos_maestro_voice_', count:10, time: 1.45, velocity: 0.8, y: 62},
-    {image: 'Music.png', file: 'bss_marcos_maestro_music_', count:10, time: 1.45, velocity: 0.8, y: 413},  
-    {image: 'Melody.png', file: 'bss_marcos_maestro_melody_', count:10, time: 1.45, velocity: 0.8, y: 722}, 
-    {image: 'Spice.png', file: 'bss_marcos_maestro_sting_', count:10, min:[0.0,2.3], max:[0.6,3.2]}
+    {image: 'BWTM.png', file: 'bss_marcos_maestro_voice_', count:30, time: 1.45, velocity: 0.8, y: 62},
+    {image: 'Music.png', file: 'bss_marcos_maestro_music_', count:30, time: 1.45, velocity: 0.8, y: 413},  
+    {image: 'Melody.png', file: 'bss_marcos_maestro_melody_', count:30, time: 1.45, velocity: 0.8, y: 722}, 
+    {image: 'Spice.png', file: 'bss_marcos_maestro_sting_', count:30, min:[0.0,2.3], max:[0.6,3.2]}
 ];
 
 config.voiceCount = config.voices.length;
